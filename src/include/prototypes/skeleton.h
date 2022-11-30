6f3ed8cf3a36466ef2471c0264c57bcda588913f
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2022-11-30 20:00:51 +0200

    range implemented

--- src/include/prototypes/skeleton.h
+++ src/include/prototypes/skeleton.h
@@ -6,6 +6,10 @@
 #include "../entity/entity.h"
 #include "../sound.h"
 
+#define _RANGE_ 4
+#define _ROOM_WORLD_LENGTH_ 12
+#define _ROOM_WORLD_WIDTH_ 7
+
 void skeleton_update(Entity* _self, World* _world, Room* _room);
 
 void skeleton_kill(Entity* _self);
--- src/prototypes/skeleton.c
+++ src/prototypes/skeleton.c
@@ -4,20 +4,53 @@ void skeleton_update(Entity* _self, World* _world, Room* _room) {
     ivec2 world_position = screenToGridPosition(_self->position);
 
     i8 direction = _world->grid.vertices[world_position.x][world_position.y];
+    i32 distance_to_player = _world->grid.distance_to_player[world_position.x][world_position.y];
 
     switch (direction)
     {
         case 'u': //Up
-            _self->position.y += 1;
+            if (distance_to_player > _RANGE_) {
+                _self->position.y += 1;
+            } else if (distance_to_player < _RANGE_) {
+                if (world_position.y - 1 >= 0) {
+                    _self->position.y -= 1;
+                } else if (world_position.x + 1 <= _ROOM_WORLD_LENGTH_){
+                    _self->position.x += 1;
+                }
+            }
             break;
         case 'r': //Right
-            _self->position.x += 1;
+            if (distance_to_player > _RANGE_) {
+                _self->position.x += 1;
+            } else if (distance_to_player < _RANGE_) {
+                if (world_position.x - 1 >= 0) {
+                    _self->position.x -= 1;
+                } else if (world_position.y - 1 >= 0) {
+                    _self->position.y -= 1;
+                }
+            }
             break;
         case 'd': //Down
-            _self->position.y -= 1;
+            if (distance_to_player > _RANGE_) {
+                _self->position.y -= 1;
+            } else if (distance_to_player < _RANGE_) {
+                if (world_position.y + 1 <= _ROOM_WORLD_WIDTH_) {
+                    _self->position.y += 1;
+                } else if (world_position.x - 1 >= 0){
+                    _self->position.x -= 1;
+                }
+            }
             break;
         case 'l': //Left
-            _self->position.x -= 1;
+            if (distance_to_player > _RANGE_) {
+                _self->position.x -= 1;
+            } else if (distance_to_player < _RANGE_) {
+                if (world_position.x + 1 <= _ROOM_WORLD_LENGTH_) {
+                    _self->position.x += 1;
+                } else if (world_position.y + 1 <= _ROOM_WORLD_WIDTH_) {
+                    _self->position.y += 1;   
+                }
+            }
             break;
         default:
             break;
